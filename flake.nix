1c4b34e96f3d749f8664ffba4e870710de362acb
superflash41 <saymonnichorayo@gmail.com>
2026-09-07 08:56:13 -0700

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "breakly env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.bun
+          pkgs.just
+        ];
+
+        shellHook = ''
+          export PATH="$PWD/node_modules/.bin:$PATH"
+        '';
+      };
+    });
+}
